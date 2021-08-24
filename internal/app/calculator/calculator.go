d3c2491dee2b8cbdf9a1508018c1dc0044e278b2
Roman <blackwanted@yandex.ru>
2021-08-24 23:06:15 +0300

    Fix -- Ssageo

--- internal/app/calculator/calculator.go
+++ internal/app/calculator/calculator.go
@@ -13,12 +13,14 @@ var DataDir string
 func Start(frontFs embed.FS) {
 	var host = flag.String("host", "127.0.0.1", "Server Hostname")
 	var port = flag.Int("port", 16000, "Server Port")
-	_ = flag.Int("clientPort", 8080, "Client Port")
+	_ = flag.Int("clientPort", 8000, "Client Port")
 	_ = flag.Bool("gui", false, "Use Gui")
+	_ = flag.Bool("initDev", false, "Install dev")
 	_ = flag.Int("width", 1100, "Window Width")
 	_ = flag.Int("height", 900, "Window Height")
-	var dataDir = flag.String("data-dir", "db", "Data Directory")
-	_ = flag.String("app-id", "id", "App id")
+	var dataDir = flag.String("dataDir", "db", "Data Directory")
+	_ = flag.String("folder", "", "Folder")
+	_ = flag.String("appId", "id", "App id")
 	flag.Parse()
 	DataDir = *dataDir
 
